54afc5fdbbaa082df33486d037acd4e87177d89b
Johnny Saniat <Jcsaniat@gmail.com>
2024-02-20 09:15:38 -0600

    Add project files.

---
+++ BangazonDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Bangazon.Models;
+using System.Runtime.CompilerServices;
+
+public class BangazonDbContext : DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+    public DbSet<PaymentType> PaymentTypes { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<ProductType> ProductTypes { get; set; }
+    public DbSet<User> Users { get; set; }
+
+
+    public BangazonDbContext(DbContextOptions<BangazonDbContext> context) : base(context)
+    {
+
+    }
+}
